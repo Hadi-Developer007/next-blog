21ecb2158166cb7b753a014956bfc429edc14baf
Hadi-Developer007 <hadideveloper006@gmail.com>
2024-06-06 02:18:05 +0330

    first init

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html dir="rtl" lang="fa-Ir">
       <body className={inter.className}>{children}</body>
     </html>
   );
